e56a620274053ae9c479b1e10808ded5a388fcfa
kabaleros <d.loboda@outlook.com>
2020-06-01 20:42:43 +0300

    HW 05, task 2 by Dmytro Loboda

---
+++ 005_Task2.py
@@ -0,0 +1,12 @@
+# Генерується список випадкових цілих чисел. Визначається, скільки в ньому парних чисел, а скільки непарних.
+# Вихідний список містить позитивні і негативні числа. Потрібно позитивні помістити в один список, а негативні - в інший.
+import random
+a = []
+for i in range(20):
+    a.append(int(random.random() * 20) - 10)
+print(a)
+
+positive = [x for x in a if x > 0]
+negative = [x for x in a if x < 0]
+print("Positive numbers are", positive)
+print("Negative numbers are", negative)
